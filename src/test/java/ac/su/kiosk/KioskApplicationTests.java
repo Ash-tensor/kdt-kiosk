640d0d0ea4338091eee294a079374d1f8cecf804
ash <Tensor17s@gmail.com>
2024-06-24 14:21:33 +0900

    initial commitment

---
+++ src/test/java/ac/su/kiosk/KioskApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.su.kiosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
