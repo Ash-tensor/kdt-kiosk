f992154137903073cf0bbc40b21186d85aca9b1a
ash <Tensor17s@gmail.com>
2024-07-02 09:24:54 +0900

    네이밍 컨벤션 통일

--- src/main/java/ac/su/kiosk/domain/Order.java
+++ src/main/java/ac/su/kiosk/domain/Order.java
@@ -10,20 +10,21 @@ import java.time.LocalDateTime;
 
 
 @Entity
-@Table(name = "Orders")
+@Table(name = "orders")
+// 테이블 엔티티는
 @Getter @Setter
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long OrderID;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "CustomerID")  // 여기서 부터는 임의로 지은 테이블 명 및 클래스 명 이용 추후 수정 예정
-//    private Customer CustomerID;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CustomerID")  // 여기서 부터는 임의로 지은 테이블 명 및 클래스 명 이용 추후 수정 예정
+    private Customer CustomerID;
 
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "KioskID")
-//    private Kiosk KioskID;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "KioskID")
+    private Kiosk KioskID;
 
     // 오류를 예방하기 위해 일단 관계없이 생성
     // 추후 위의 JoinColumn 사용
