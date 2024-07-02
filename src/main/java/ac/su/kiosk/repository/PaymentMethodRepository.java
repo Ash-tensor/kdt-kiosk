dbbeeda645a0abdb574b287bfb6d35b4e633dd66
HyojinBae <hyojin9180@gmail.com>
2024-07-02 10:46:28 +0900

    Payment, PaymentMethod Repository 구현

---
+++ src/main/java/ac/su/kiosk/repository/PaymentMethodRepository.java
@@ -0,0 +1,13 @@
+package ac.su.kiosk.repository;
+
+import ac.su.kiosk.domain.Order;
+import ac.su.kiosk.domain.PaymentMethod;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PaymentMethodRepository extends JpaRepository<Order, Long> {
+    List<PaymentMethod> findByPaymentMethodID(Long paymentMethodID);
+}
