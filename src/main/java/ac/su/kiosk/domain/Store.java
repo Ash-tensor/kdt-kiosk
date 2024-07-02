e0dafca171f49f0f387715c81bf51f971408aa99
goeun lee <ccccathy24@gmail.com>
2024-07-02 14:25:33 +0900

    스토어,키오스크,애드민 도메인 수정

--- src/main/java/ac/su/kiosk/domain/Store.java
+++ src/main/java/ac/su/kiosk/domain/Store.java
@@ -1,10 +1,14 @@
 package ac.su.kiosk.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
+@Getter @Setter
+@Table(name="store")
 public class Store {
     @Id //이 클래스의 기본 키
     @GeneratedValue(strategy = GenerationType.IDENTITY)//기본 키 필드의 값이 데이터베이스의 자동 증가 기능을 사용하여 생성
