cfad811a09ce9b38b9b4c426abd820fa7a5cc656
goeun lee <ccccathy24@gmail.com>
2024-07-02 13:39:56 +0900

    스토어,키오스크,애드민 도메인 수정

--- src/main/java/ac/su/kiosk/domain/Store.java
+++ src/main/java/ac/su/kiosk/domain/Store.java
@@ -10,43 +10,11 @@ public class Store {
     @GeneratedValue(strategy = GenerationType.IDENTITY)//기본 키 필드의 값이 데이터베이스의 자동 증가 기능을 사용하여 생성
     private int storeID;
 
+    @Column
     private String storeName;
+    @Column
     private String location;
 
     @OneToMany(mappedBy = "store")//일대다 관계
     private List<Kiosk> kiosks;
-
-    //Getters and setters
-
-    public int getStoreID() {
-        return storeID;
-    }
-
-    public void setStoreID(int storeID) {
-        this.storeID = storeID;
-    }
-
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public List<Kiosk> getKiosks() {
-        return kiosks;
-    }
-
-    public void setKiosks(List<Kiosk> kiosks) {
-        this.kiosks = kiosks;
-    }
 }
