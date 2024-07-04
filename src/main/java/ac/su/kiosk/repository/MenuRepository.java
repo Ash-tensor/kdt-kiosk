9ea6a2919a2f0cf17541ba160491f9112b43acb2
Dyumell <sorbet0231@gmail.com>
2024-07-04 16:35:15 +0900

    메뉴, 카테고리 서비스,레포지토리, 카테고리 컨트롤러 구현

--- src/main/java/ac/su/kiosk/repository/MenuRepository.java
+++ src/main/java/ac/su/kiosk/repository/MenuRepository.java
@@ -3,14 +3,11 @@ package ac.su.kiosk.repository;
 import ac.su.kiosk.domain.Category;
 import ac.su.kiosk.domain.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface MenuRepository extends JpaRepository<Menu,Integer>, QuerydslPredicateExecutor<Menu>
-{
-    Optional<Menu> findById(Long id);
+public interface MenuRepository extends JpaRepository<Menu, Integer> {
+    List<Menu> findAllByCategory(Category category);
 }
--- src/main/java/ac/su/kiosk/service/MenuService.java
+++ src/main/java/ac/su/kiosk/service/MenuService.java
@@ -1,23 +1,30 @@
 package ac.su.kiosk.service;
 
+import ac.su.kiosk.domain.Category;
 import ac.su.kiosk.domain.Menu;
+import ac.su.kiosk.repository.CategoryRepository;
 import ac.su.kiosk.repository.MenuRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class MenuService {
+    private final MenuRepository menuRepository;
+    private final CategoryRepository categoryRepository;
 
-    @Autowired
-    private MenuRepository menuRepository;
-
-    public Optional<Menu> getMenusByCategory(Long id) {
-        return menuRepository.findById(id);
+    public List<Menu> getAll() {
+        return menuRepository.findAll();
     }
 
-    public Menu getMenu(Long id) {
-        return menuRepository.findById(id).orElse(null);
+    public List<Menu> getAllByCategory(Category category) {
+        return menuRepository.findAllByCategory(category);
+    }
+
+    public Optional<Category> getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
     }
 }
