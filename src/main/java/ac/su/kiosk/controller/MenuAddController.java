3aa75ab3419abd2735d938521fe45ec9c62d73d6
HyojinBae <hyojin9180@gmail.com>
2024-07-04 17:41:07 +0900

    Payment, PaymentMethod Repository 구현

--- src/main/java/ac/su/kiosk/controller/MenuAddController.java
+++ src/main/java/ac/su/kiosk/controller/MenuAddController.java
@@ -1,28 +1,33 @@
-//package ac.su.kiosk.controller;
-//
-//import ac.su.kiosk.service.MenuService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@RequiredArgsConstructor
-//@RequestMapping("/admin/menu")
-//@Controller
-//public class MenuAddController {
-//    private final MenuService menuService;
-//
-//    @GetMapping("/add")
-//    public String addMenu() {
-//        return "admin/menu/add";
-//    }
-//
-//    @PostMapping("/add")
-//    public String addMenu(String menuDescription, String menuName, int categoryID, int basePrice) {
-//        menuService.addMenu(menuDescription, menuName, categoryID, basePrice);
-//        return "redirect:/admin/menu/add";
-//    }
-//
-//
-//}
+package ac.su.kiosk.controller;
+
+import ac.su.kiosk.domain.Menu;
+import ac.su.kiosk.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@RequestMapping("/admin/menu")
+@Controller
+public class MenuAddController {
+    private final MenuService menuService;
+
+    @GetMapping("/add")
+    public String viewAddMenu(){return "admin/menu/add";}
+
+    @PostMapping("/add")
+    public void addMenu(String menuDescription, MultipartFile file,
+                          String menuName, int categoryID, int basePrice, Model model) throws IOException {
+        menuService.addMenu(menuDescription, file, menuName, categoryID, basePrice);
+        model.addAttribute("menuName", menuName);
+
+    }
+
+
+}
