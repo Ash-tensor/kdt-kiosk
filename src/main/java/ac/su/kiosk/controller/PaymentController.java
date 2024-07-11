d45ce6d12ded61f269ab4347cf21a7319f0fd98d
SPAMMAYO <nucon84@gmail.com>
2024-07-11 15:22:15 +0900

    PaymentController를 RestController로 변경

--- src/main/java/ac/su/kiosk/controller/PaymentController.java
+++ src/main/java/ac/su/kiosk/controller/PaymentController.java
@@ -11,29 +11,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+//@Controller
+@RestController
 public class PaymentController {
     private final PaymentService paymentService;
+
     @GetMapping("/payment")
-    public String paymentPage(Model model) {
+    public ResponseEntity<OrderModuleDTO> getPaymentInfo() {
         OrderModuleDTO requestDto = paymentService.makeOrderModuleDTO();
-        model.addAttribute("requestDto", requestDto);
-        return "test/payment";
+        return new ResponseEntity<>(requestDto, HttpStatus.OK);
     }
 
-    @ResponseBody
     @PostMapping("/payment")
-    public ResponseEntity<IamportResponse<Payment>> validationPayment(@RequestBody OrderModuleDTO request) {
-        //OrderModuleDTO request =  testService.makeOrderModuleDTO();
-        IamportResponse<Payment> IamportResponse = paymentService.paymentByCallback(request);
-        log.info("결제 응답 = {}", IamportResponse.getResponse().toString());
-        return new ResponseEntity<>(IamportResponse, HttpStatus.OK);
+    public ResponseEntity<IamportResponse<Payment>> validatePayment(@RequestBody OrderModuleDTO request) {
+        IamportResponse<Payment> iamportResponse = paymentService.paymentByCallback(request);
+        log.info("결제 응답 = {}", iamportResponse.getResponse().toString());
+        return new ResponseEntity<>(iamportResponse, HttpStatus.OK);
     }
+
+//    @GetMapping("/payment")
+//    public String paymentPage(Model model) {
+//        OrderModuleDTO requestDto = paymentService.makeOrderModuleDTO();
+//        model.addAttribute("requestDto", requestDto);
+//        return "test/payment";
+//    }
+
+//    @ResponseBody
+//    @PostMapping("/payment")
+//    public ResponseEntity<IamportResponse<Payment>> validationPayment(@RequestBody OrderModuleDTO request) {
+//        //OrderModuleDTO request =  testService.makeOrderModuleDTO();
+//        IamportResponse<Payment> IamportResponse = paymentService.paymentByCallback(request);
+//        log.info("결제 응답 = {}", IamportResponse.getResponse().toString());
+//        return new ResponseEntity<>(IamportResponse, HttpStatus.OK);
+//    }
 }
