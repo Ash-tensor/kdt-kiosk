f562e71be0f296e42d4782ebc89711a7559e46c6
lys0611 <dldptmd611@gmail.com>
2024-07-12 14:36:47 +0900

    메뉴 옵션 삭제

--- src/main/java/ac/su/kiosk/domain/Menu.java
+++ src/main/java/ac/su/kiosk/domain/Menu.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Entity
 @Getter @Setter
 @Table(name = "menu")
@@ -30,11 +28,6 @@ public class Menu {
     @ManyToOne
     @JoinColumn(name = "categoryId", nullable = false)
     private Category category;
-
-    @ElementCollection
-    @CollectionTable(name = "menu_options", joinColumns = @JoinColumn(name = "menu_id"))
-    @Column(name = "option")
-    private List<String> options;
 
     @Column
     private boolean soldOut;
