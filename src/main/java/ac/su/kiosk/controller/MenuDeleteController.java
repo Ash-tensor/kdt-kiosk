752304d9e084ddb9256ac3b180ea0094dfe2197d
msaki <msaki061@gmail.com>
2024-07-15 02:57:36 +0900

    상품 관리 관련 백엔드 수정

--- src/main/java/ac/su/kiosk/controller/MenuDeleteController.java
+++ src/main/java/ac/su/kiosk/controller/MenuDeleteController.java
@@ -2,17 +2,16 @@ package ac.su.kiosk.controller;
 
 import ac.su.kiosk.service.MenuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/admin/menu")
 public class MenuDeleteController {
     private final MenuService menuService;
 
-    @DeleteMapping("/admin/menu/delete")
-    public void deleteMenu(@RequestParam int id) {
+    @DeleteMapping("/delete/{id}")
+    public void deleteMenu(@PathVariable int id) {
         menuService.deleteMenu(id);
     }
 }
--- src/main/java/ac/su/kiosk/service/MenuUpdateService.java
+++ src/main/java/ac/su/kiosk/service/MenuUpdateService.java
@@ -1,44 +1,44 @@
-package ac.su.kiosk.service;
+// MenuService 통합.
 
-import ac.su.kiosk.domain.Menu;
-import ac.su.kiosk.repository.MenuRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@RequiredArgsConstructor
-@Service
-public class MenuUpdateService {
-    private final MenuRepository menuRepository;
-
-    public List<Menu> getAll() {
-        return menuRepository.findAll();
-    }
-
-    public List<Menu> getMenusByCategory(Long categoryId) {
-        return menuRepository.findByCategoryId(categoryId);
-    }
-
-    public Menu addMenu(Menu menu) {
-        return menuRepository.save(menu);
-    }
-
-    public Menu updateMenu(int id, Menu menu) {
-        Menu existingMenu = menuRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid menu ID"));
-        existingMenu.setName(menu.getName());
-        existingMenu.setPrice(menu.getPrice());
-        existingMenu.setDescription(menu.getDescription());
-        existingMenu.setImage(menu.getImage());
-        existingMenu.setCategory(menu.getCategory());
-        // options 필드를 제거한 후 수정된 코드
-        // existingMenu.setOptions(menu.getOptions()); // 이 줄을 제거합니다.
-        existingMenu.setSoldOut(menu.isSoldOut());
-        existingMenu.setTag(menu.getTag());
-        return menuRepository.save(existingMenu);
-    }
-
-    public void deleteMenu(int id) {
-        menuRepository.deleteById(id);
-    }
-}
+//package ac.su.kiosk.service;
+//
+//import ac.su.kiosk.domain.Menu;
+//import ac.su.kiosk.repository.MenuRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@RequiredArgsConstructor
+//@Service
+//public class MenuUpdateService {
+//    private final MenuRepository menuRepository;
+//
+////    public List<Menu> getAll() {
+////        return menuRepository.findAll();
+////    }
+////
+////    public List<Menu> getMenusByCategory(Long categoryId) {
+////        return menuRepository.findByCategoryId(categoryId);
+////    }
+////
+////    public Menu addMenu(Menu menu) {
+////        return menuRepository.save(menu);
+////    }
+//
+//    public Menu updateMenu(int id, Menu menu) {
+//        Menu existingMenu = menuRepository.findById(id)
+//                .orElseThrow(() -> new RuntimeException("Menu not found with id: " + id));
+//        existingMenu.setName(menu.getName());
+//        existingMenu.setCategory(menu.getCategory());
+//        existingMenu.setPrice(menu.getPrice());
+//        existingMenu.setSoldOut(menu.isSoldOut());
+//        existingMenu.setTag(menu.getTag());
+//        return menuRepository.save(existingMenu);
+//    }
+//
+////    public void deleteMenu(int id) {
+////        menuRepository.deleteById(id);
+////    }
+//}
