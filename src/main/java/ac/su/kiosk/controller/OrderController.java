680acb14f96f7f8cfcb3c9aeebe3445664482628
HyojinBae <hyojin9180@gmail.com>
2024-07-16 09:30:59 +0900

    payment save

--- src/main/java/ac/su/kiosk/controller/OrderController.java
+++ src/main/java/ac/su/kiosk/controller/OrderController.java
@@ -1,6 +1,10 @@
 package ac.su.kiosk.controller;
 
+import ac.su.kiosk.constant.PaymentStatus;
 import ac.su.kiosk.domain.Order;
+import ac.su.kiosk.domain.OrderModuleDTO;
+import ac.su.kiosk.dto.IAMPortDTO;
+import ac.su.kiosk.repository.OrderModuleDTORepository;
 import ac.su.kiosk.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +19,20 @@ import org.springframework.web.bind.annotation.*;
 public class OrderController {
 
     private final OrderService orderService;
+    private final OrderModuleDTORepository orderModuleDTORepository;
 
     @PostMapping
     public ResponseEntity<Order> createOrder(@RequestBody Order order) {
         Order savedOrder = orderService.createOrder(order);
         return new ResponseEntity<>(savedOrder, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/iamPortDto")
+    public void createIamPortDto(@RequestBody IAMPortDTO iamPortDTO) {
+        OrderModuleDTO orderModuleDTO = orderModuleDTORepository.findByOrderUid(iamPortDTO.getOrderUid()).get();
+        orderModuleDTO.setPaymentUid(iamPortDTO.getPaymentUid());
+        orderModuleDTO.setPrice(iamPortDTO.getPrice());
+        orderModuleDTO.setStatus(PaymentStatus.OK);
+        orderModuleDTORepository.save(orderModuleDTO);
     }
 }
